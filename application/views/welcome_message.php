dd200dab730980e35bd012f6bad02958194608d3
Nolfneo <igorok901@gmail.com>
2015-10-31 12:42:18 +0200

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@
 
 <div id="container">
 	<h1>Welcome to CodeIgniter!!!</h1>
-	<?= 123?>
+	<?= 1234?>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
