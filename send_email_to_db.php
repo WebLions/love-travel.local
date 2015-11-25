5b8ae2431f42501f44d906117252bee347d47ed3
Satuyan <ragnolur@gmail.com>
2015-11-25 22:46:37 +0200

    Version 3.60
    Added new "send_email" by @Nolfneo

--- send_email_to_db.php
+++ send_email_to_db.php
@@ -3,18 +3,34 @@ function send_email_to_db($email){
 	// Ваш ключ доступа к API
 	$api_key = "3e8d76dc9e9b4f7e84c7cbaf76b1ef6e";//Поменяй на нужный апи, который тебе выдаст сервис
 	//Имя метода
-	$method = "subscriberImport";
+	$method = "listSubscribeOptInNow";
 	//параметры
+	$list_id = "94138";
 	$phone = "";
 	$fname = "";
 	$lname = "";
-	$parameters = "apikey=$api_key&email=$email&phone=$phone&mobilecountry=$mobilecountry&fname=$fname&lname=$lname&names=&values=&update_existing=true";
+	$mobilecountry = "";
+	$fname = "";
+	$lname = "";
+	$parameters = "apikey=$api_key&list_id=$list_id&email=$email&phone=&mobilecountry=&fname=&lname=&names=&values=&optin=TRUE&update_existing=TRUE";
 	// Создаём GET-запрос
-	$api_url = "http://api.feedgee.com/1.0/$method?$parameters&output=json";
+	$api_url = "http://api.feedgee.com/1.0/$method?$parameters";
+	 
 	// Делаем запрос на API-сервер
-	$result = file_get_contents($api_url);
-	if(!empty($result))
-		return true;
-	else
-		return false;
+	    if( $curl = curl_init() ) {
+        $uagent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)";
+        curl_setopt($curl, CURLOPT_URL, $api_url);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); 
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION,1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER,1); 
+        curl_setopt($curl, CURLOPT_ENCODING, "gzip, deflate");
+        curl_setopt($curl, CURLOPT_USERAGENT, $uagent);
+        curl_setopt($curl, CURLOPT_COOKIEJAR, "coo.txt");
+        curl_setopt($curl, CURLOPT_COOKIEFILE, "coo.txt");
+        $out = curl_exec($curl);
+        curl_close($curl);
+    }     
+	
 }
